f31aec37bfceb3393ee0f681769fb8041c2a9a9f
rofik <rofiksupriant@gmail.com>
2022-10-28 19:31:35 +0700

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/routes/home/home.component';
+import Navigation from './components/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import Shop from './routes/shop/home.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home/>} />
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home/>} />
+        <Route path='/shop' element={<Shop/>} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/routes/shop/home.component.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+const Shop = () => {
+    return (
+        <Fragment>
+            <h1>This is Shop page</h1>
+        </Fragment>
+    );
+}
+
+export default Shop;
